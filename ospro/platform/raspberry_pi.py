3456a2dc278fd2db968d3bcd17615a7f5d853157
Thomas Eleff <tome18@comcast.net>
2024-10-21 12:19:45 -0500

    Resolves runtime error #30

--- ospro/platform/raspberry_pi.py
+++ ospro/platform/raspberry_pi.py
@@ -5,6 +5,17 @@ from ospro import exceptions
 # Import raspberry-pi general-purpose input / output interface.
 try:
     import RPi.GPIO as GPIO
+
+    # Define the board mode
+    if not GPIO.getmode():
+        GPIO.setmode(GPIO.BCM)
+    elif GPIO.getmode() == 10:
+        raise exceptions.InvalidModeError(
+            'Invalid interface mode {BOARD}.'
+        )
+    else:
+        pass
+
 except ImportError:
     raise exceptions.InvalidPlatformError(
         ' '.join([
